6d730f6817fed515800b853893f336f736c8e1c6
Naimisha Badri <starzblue24@gmail.com>
2014-12-11 21:45:02 -0700

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,1 +1,62 @@
 // class to represent an emplayee - inherits Person
+public class Employee extends Person
+{
+  private double hourlyWage;
+  private int hoursWorked;
+  private String jobTitle;
+  private Manager man;
+  public Employee(String nameF, String nameL, int agee, double wage, int hours, String title)
+  {
+    super(nameF, nameL, agee);;
+    hourlyWage=wage;
+    hoursWorked=hours;
+    jobTitle=title;
+  }
+  public double getWage()
+  {
+    return hourlyWage;
+  }
+  public int hoursWorked()
+  {
+    return hoursWorked;
+  }
+  public double getWeeklySalary()
+  {
+  return  hoursWorked*=hourlyWage;
+  }
+  public double getYearlySalary()
+  {
+    double yearlySalary=    hoursWorked*hourlyWage;
+    yearlySalary*=52;
+    return yearlySalary;
+  }
+  public String getJobTitle()
+  {
+     return jobTitle;
+  }
+  public String toString()
+  {
+    return super.toString()+" :"+jobTitle+"@ "+getYearlySalary();
+  }
+  public void setWage(double newWage)
+  {
+    hourlyWage=newWage;
+  }
+  public void setHours(int h)
+  {
+    hoursWorked=h;
+  }
+  public void setJobTitle(String title)
+  {
+    jobTitle=title;
+  }
+  public void giveRaise()
+  {
+    hourlyWage+=1;
+  }
+  public void setManager(Manager newManager)
+  {
+    man=newManager;
+  }
+}
+  
