b091ff189920213cdf2f235737a62d8b789633fc
Naimisha Badri <starzblue24@gmail.com>
2014-12-11 21:46:25 -0700

    Update Person.java

--- src/Person.java
+++ src/Person.java
@@ -1,1 +1,39 @@
 // Super Class to represent a person
+public class Person
+{
+  private String firstName;
+  private String lastName;
+  private int age;
+  public Person()
+  {
+    firstName="";
+    lastName="";
+    age=0;
+  }
+  public Person(String nameF, String nameL, int agee)
+  {
+    firstName=nameF;
+    lastName=nameL;
+    age=agee;
+  }
+  public String getFirstName()
+  {
+    return firstName;
+  }
+  public String getLastName()
+  {
+    return lastName;
+  }
+  public int getAge()
+  {
+    return age;
+  }
+  public String toString()
+  {
+    return lastName+", "+firstName;
+  }
+  public void recordBirthday()
+  {
+    age+=1;
+  }
+}
